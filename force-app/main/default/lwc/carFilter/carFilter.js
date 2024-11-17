0f2faf8de122f9e1b3914cc04e607c18ed967ce3
tanmaychandak <tanmaychandak13@gmail.com>
2024-11-17 23:18:58 +0530

    Lightning message services and Filtering out cars.

--- force-app/main/default/lwc/carFilter/carFilter.js
+++ force-app/main/default/lwc/carFilter/carFilter.js
@@ -1,3 +1,77 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import CAR_OBJECT from '@salesforce/schema/Car__c'
 
-export default class CarFilter extends LightningElement {}
+import CATEGORY_FIELD from '@salesforce/schema/Car__c.Category__c'
+import MAKE_FIELD from '@salesforce/schema/Car__c.Make__c'
+
+const CATEGORY_ERROR = 'Error loading categories'
+const MAKE_ERROR = 'Error loading make Types'
+
+import CARS_FILTERED_MESSAGE from '@salesforce/messageChannel/CarsFiltered__c'
+import { publish, MessageContext } from 'lightning/messageService'
+
+
+export default class CarFilter extends LightningElement {
+    filters={
+        searchKey:'',
+        maxPrice:999999
+    }
+    timer
+    categoryError = CATEGORY_ERROR
+    makeError = MAKE_ERROR
+
+    @wire(MessageContext)
+    messageContext
+
+
+    @wire(getObjectInfo, {objectApiName:CAR_OBJECT})
+    carObjectInfo
+
+    @wire(getPicklistValues, {
+        recordTypeId:'$carObjectInfo.data.defaultRecordTypeId',
+        fieldApiName:CATEGORY_FIELD
+    })categories
+
+    @wire(getPicklistValues, { recordTypeId:'$carObjectInfo.data.defaultRecordTypeId', fieldApiName:MAKE_FIELD})
+    makeType
+
+    handleSearchKeyChange(event){
+        console.log(event.target.value)
+        this.filters = {...this.filters, "searchKey":event.target.value}
+        this.sendDataToCarList()
+    }
+
+    handleMaxPriceChange(event){
+        console.log(event.target.value)
+        this.filters = {...this.filters, "maxPrice":event.target.value} 
+        this.sendDataToCarList()
+    }
+
+    handleCheckbox(event){
+        if(!this.filters.categories){
+            const categories = this.categories.data.values.map(item=>item.value)
+            const makeType = this.makeType.data.values.map(item=>item.value)
+            this.filters = {...this.filters, categories, makeType}
+        }
+        const {name, value} = event.target.dataset
+        if(event.target.checked){
+            if(!this.filters[name].includes(value)){
+                this.filters[name] = [...this.filters[name], value]
+            }
+        } else {
+            this.filters[name] =  this.filters[name].filter(item=>item !==value)
+        }
+        this.sendDataToCarList()
+    }
+
+    sendDataToCarList(){
+        window.clearTimeout(this.timer)
+        this.timer = window.setTimeout(()=>{
+            publish(this.messageContext, CARS_FILTERED_MESSAGE, {
+                filters:this.filters
+            })
+        }, 400)
+    }
+
+}
--- force-app/main/default/lwc/carTileList/carTileList.js
+++ force-app/main/default/lwc/carTileList/carTileList.js
@@ -1,3 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement , wire } from 'lwc';
+import getCars from '@salesforce/apex/CarController.getCars'
 
-export default class CarTileList extends LightningElement {}
+import CARS_FILTERED_MESSAGE from '@salesforce/messageChannel/CarsFiltered__c'
+import { subscribe, MessageContext } from 'lightning/messageService'
+
+export default class CarTileList extends LightningElement {
+
+    cars
+    error
+    filters = {};
+    carFilterSubscription
+
+    @wire(getCars,{filters:'$filters'})
+    carsHandler({data,error}){
+        if(data){
+            console.log(data);
+            this.cars = data
+        }
+        if(error){
+            console.error(error);
+            this.error = error
+        }
+    }
+
+    @wire(MessageContext)
+    messageContext
+    
+    connectedCallback(){
+        this.subscribeHandler()
+    }
+
+    subscribeHandler(){
+        this.carFilterSubscription = subscribe(this.messageContext,CARS_FILTERED_MESSAGE,(message)=>this.handleFilterChanges(message))
+    }
+
+    handleFilterChanges(message){
+        console.log(message.filters);
+        this.filters ={...message.filters}
+    }
+
+}
